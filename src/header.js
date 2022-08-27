16da761b3ce73eacc9623576f4dc84cda1247cfc
Carlos Marin <devcarlosmarin@gmail.com>
2022-08-27 13:25:08 +0200

    Starting the nav bar

--- src/header.js
+++ src/header.js
@@ -1,17 +1,87 @@
-export default function createHeader() {
-  const element = document.createElement("nav");
-  const h1 = document.createElement('h1');
-//Titulo
+import Logo from "./img/logo.png";
 
-  element.setAttribute('class','bg-sky-500');
-  h1.innerHTML = "Restaurante pepe";
-h1.setAttribute('class','text-3xl underline flex items-cente');  
-  element.appendChild(h1);
+export default function createnavBar() {
+  //Creamos la NavBar
+  const navBar = document.createElement("nav");
+  //Creamos el div contenedor
+  const container = document.createElement("div");
 
+  //Seteamos los atributos del elemento nav
+  navBar.setAttribute(
+    "class",
+    "bg-red-600 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900"
+  );
+  //Seteamos los atributos del div contenedor
+  container.setAttribute(
+    "class",
+    "container flex flex-wrap justify-between items-center mx-auto"
+  );
+  //Creamos un hipervinculo que nos lleve al inicio
+  const a = document.createElement("a");
+  //Aqui lo hay que implementar la funcion que nos ponga el estado por default, ya que todo esta generado por JS no hay un url.
+  a.setAttribute("class", "flex items-center");
 
+  //Insertamos el logo en el hipervinculo
+  const logo = new Image();
+  logo.src = Logo;
+  logo.setAttribute("class", "mr-16 h-24 sm:h-30");
+  a.appendChild(logo);
+  const span = document.createElement("span");
+  span.innerHTML = "Los Pollos Hermanos";
+  span.setAttribute(
+    "class",
+    "text-white self-center text-4xl font-semibold whitespace-nowrap dark:text-white"
+  );
+  a.appendChild(span);
+  //Creamos los botones
 
+  //Creamos el div que contiene los botones
+  const listDiv = document.createElement("div");
+  listDiv.setAttribute("class", "hidden w-full md:block md:w-auto");
+  listDiv.setAttribute("id", "navbar-default");
+  //Creamos la lista
+  const list = document.createElement("ul");
+  list.setAttribute(
+    "class",
+    "flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+  );
+  //Creamos los botones.
+  //Home
+  const liHome = document.createElement("li");
+  list.appendChild(liHome);
+  const homeButton = document.createElement("button");
+  homeButton.innerHTML = "Inicio";
+  homeButton.setAttribute(
+    "class",
+    "flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+  );
+  liHome.appendChild(homeButton);
+//Menu
+  const liMenu = document.createElement("li");
+  list.appendChild(liMenu);
+  const menuButton = document.createElement("button");
+  menuButton.innerHTML = "Menu";
+  menuButton.setAttribute(
+    "class",
+    "flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+  );
+  liMenu.appendChild(menuButton);
+//Contacto
 
-  return element;
+  const liContacto = document.createElement("li");
+  list.appendChild(liContacto);
+  const contactoButton = document.createElement("button");
+  contactoButton.innerHTML = "contacto";
+  contactoButton.setAttribute(
+    "class",
+    "flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+  );
+  liMenu.appendChild(contactoButton);
+
+  listDiv.appendChild(list);
+  container.appendChild(a);
+  container.appendChild(listDiv);
+
+  navBar.appendChild(container);
+  return navBar;
 }
-
-
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import './style.css';
-import createHeader from './header.js';
+import createnavBar from './header.js';
 import _ from 'lodash';
+
 //En el index simplemente crear una funcion que sea crearWeb()
 //Crear otro archivo js que inicialice todas las funciones.
 //Crear un archivo para cada funcionalidad.
 
 
-
 const container = document.getElementById('container')
-const header = container.appendChild(createHeader());
+const header = container.appendChild(createnavBar());
 
-const button1 = document.createElement('button');
-button1.innerHTML = 'Menu';
-header.appendChild(button1);
-button1.setAttribute('class','bg-sky-500 hover:bg-sky-700 ...');
-button1.addEventListener("click",()=>(
-    console.log('Carlos')));
